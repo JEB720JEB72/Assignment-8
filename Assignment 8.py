ef0987e7330d75a062bfac805ea2fd339eb22126
JEB720JEB72 <bakerj50@mail.lcc.edu>
2025-11-20 12:43:43 -0500

    Add Assignment 8 python file

---
+++ Assignment 8.py
@@ -0,0 +1,16 @@
+#main(): is for information gathering to get a number
+#parse_input(): will figure out if what's given is hex or decimal
+#convert_to_binary: will convert the above number to binary
+
+
+def main():
+    #abc
+    pass
+
+def parse_input():
+    #xyz
+    pass
+
+def convert_to_binary ():
+    #abc
+    pass
